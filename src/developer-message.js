6ec00838926db1d33de127bb0bb29cb015e46bbf
mb1721 <masonbennett1721@gmail.com>
2023-11-22 15:17:57 -0500

    Update developer message.

--- src/developer-message.js
+++ src/developer-message.js
@@ -1,10 +1,12 @@
 export default function developerMessage() {
   const steps = [
-    '1. /src/pages/Home.js\n\tDelete Ln 3 and Ln 8.\n',
-    '2. /src/components/\n\tDelete HomeComponent/ and Sample/.\n',
-    '3. /src/App.js\n\tAdjust imports on Ln 6 - 10 to reflect /src/pages/.\n\tAdjust routes on Ln 25 - 29 to reflect /src/pages/.\n',
-    '4. /src/index.tsx\n\tDelete Ln 5 and Ln 6.\n',
-    '5. /src/developer-message.js\n\tDelete this file.\n'
+    '1. /package.json\n\tEdit "name", "description", and "author".\n',
+    '2. /webpack.common.js\n\tEdit module.plugins.HtmlWebpackPlugin.title\n',
+    '2. /src/pages/Home.js\n\tDelete Ln 3 and Ln 8.\n',
+    '3. /src/components/\n\tDelete HomeComponent/ and Sample/.\n',
+    '4. /src/App.js\n\tAdjust imports on Ln 6 - 10 to reflect /src/pages/.\n\tAdjust routes on Ln 25 - 29 to reflect /src/pages/.\n',
+    '5. /src/index.tsx\n\tDelete Ln 5 and Ln 6.\n',
+    '6. /src/developer-message.js\n\tDelete this file.\n'
   ];
   const devSteps = steps.join('\n');
   if (process.env.NODE_ENV !== 'production') {
