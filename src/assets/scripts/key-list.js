9d27178945b17f72c5a965840c4af3df75567d89
mb1721 <masonbennett1721@gmail.com>
2023-11-22 10:51:50 -0500

    Fix tohex() bug

--- src/assets/scripts/key-list.js
+++ src/assets/scripts/key-list.js
@@ -1,17 +1,63 @@
 export default class KeyList {
+  #keys;
+  #numKeys;
   constructor(newList = []) {
-    this.keys = {};
-    this.numKeys = 0;
+    this.#keys = {};
+    this.#keys.numKeys = 0;
     newList.forEach(definedKey => this.ammendList(definedKey));
   }
 
-  get keyMap() {
-    return this.keys;
+  get keys() {
+    return this.#keys;
+  }
+
+  get size() {
+    return this.#numKeys;
   }
 
   ammendList(newKey) {
-    this.keys[newKey] = {instances: 1};
-    this.numKeys++;
+    this.#keys[newKey] = {
+      instances: [newKey],
+      count: 1
+    };
+    this.#keys.numKeys++;
+  }
+
+  toHex(value) {
+    let hexArr = [];
+    let remainders = [];
+    let power = 0;
+    let bit;
+    let divisor;
+    let hexStr = '';
+    const exponentOf16Divisor = () => {
+      let exponentOf16 = 16 ** power;
+      while (value > exponentOf16) { exponentOf16 = 16 ** ++power; } 
+      power--;
+      exponentOf16 = 16 ** power--;
+      return exponentOf16;
+    };
+    do {
+      divisor = exponentOf16Divisor();
+      bit = Math.floor(value / divisor);
+      bit >= 1 ? hexArr.push(bit) : hexArr.push(value % divisor);
+      value = value % divisor;
+      
+      remainders.push(value);
+    } while (value > 0);
+  
+    hexArr.forEach(hex => {
+      switch (hex) {
+        case 10: hexStr += 'a'; break;
+        case 11: hexStr += 'b'; break;
+        case 12: hexStr += 'c'; break;
+        case 13: hexStr += 'd'; break;
+        case 14: hexStr += 'e'; break;
+        case 15: hexStr += 'f'; break;
+        default: hexStr += hex.toString();
+      }
+    });
+    return hexStr;
   }
 
   unicodeKey(repeatKey) {
@@ -21,13 +67,15 @@ export default class KeyList {
 
   generateKey(unique) {
     unique = unique.toString();
-    const exists = this.keys[unique] ? true : false;
+    const exists = this.#keys[unique] ? true : false;
     if (!exists) {
       this.ammendList(unique);
       return unique;
     } else {
-      const unicode = this.unicodeKey(unique) * ++this.keys[unique].instances;
-      const modifiedRepeatKey = unique + unicode.toString();
+      const unicode = this.unicodeKey(unique) * ++this.#keys[unique].count;
+      const modifiedRepeatKey = unique + '#' + this.toHex(unicode);
+
+      this.#keys[unique].instances.push(modifiedRepeatKey);
       return modifiedRepeatKey; 
     }
   }
