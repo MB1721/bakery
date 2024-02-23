36c94f6e78b5b0be9c0a267298f22fb326043806
mb1721 <masonbennett1721@gmail.com>
2024-02-23 10:05:24 -0500

    Adjust package scripts

--- assets/scripts/package/clean-view.js
+++ assets/scripts/package/clean-view.js
@@ -5,6 +5,10 @@ switch (platform) {
   case 'darwin':
   case 'linux':
     exec("rm -r dist/*");
+    break;
   case 'win32':
     exec("rmdir /S /Q dist\\*");
+    break;
+  default: 
+    throw new Error('Unsupported platform');
 }
--- assets/scripts/package/rm-dependencies.js
+++ assets/scripts/package/rm-dependencies.js
@@ -5,6 +5,10 @@ switch (platform) {
   case 'darwin':
   case 'linux':
     exec("rm -r views/*/node_modules views/*/package-lock.json node_modules package-lock.json");
+    break;
   case 'win32':
     exec("del /S views\\*\\node_modules && del views\\*\\package-lock.json && del /S node_modules && del package-lock.json");
+    break;
+  default: 
+    throw new Error('Unsupported platform');
 }
