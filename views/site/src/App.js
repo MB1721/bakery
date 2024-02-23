82596919f53cfa0e19b098183b7ebc2f4783f5cc
mb1721 <masonbennett1721@gmail.com>
2024-02-23 06:46:38 -0500

    Add comments to app component

--- views/site/src/App.js
+++ views/site/src/App.js
@@ -11,12 +11,12 @@ import Terms from './pages/Terms';
 
 export default function App() {
   let pages = [];
-  const pagesDir = require.context('./pages', true, /\.(js|ts|tsx|jsx)$/);
+  const pagesDir = require.context('./pages', true, /\.(js|ts|tsx|jsx)$/); // capture javascript files
   pagesDir.keys().forEach(key => {
-    const {casedFileName} = camelCaseContext(key, ['js', 'ts', 'tsx', 'jsx']);
+    const { casedFileName } = camelCaseContext(key, ['js', 'ts', 'tsx', 'jsx']);
     pages.push(casedFileName.toLowerCase());
   });
-  pages.splice(pages.indexOf('error404'), 1);
+  pages.splice(pages.indexOf('error404'), 1); // remove error404 from the pages array
 
   const appRoute = "/site/";
 
