53bd8a28dcc1bead1e88c4929fe88be75ce6d352
mb1721 <masonbennett1721@gmail.com>
2024-02-28 08:32:30 -0500

    Ready app for development

--- views/site/src/index.tsx
+++ views/site/src/index.tsx
@@ -2,8 +2,6 @@ import '@styles/global-styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import developerMessage from './developer-message.js';
-developerMessage(); // indicate that app is ready for development
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<App/>);
