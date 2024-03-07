5164615f83fa192a4dd6fddc6e385d1ba22c23a1
mb1721 <masonbennett1721@gmail.com>
2024-03-07 00:02:13 -0500

    Start header customization

--- views/site/src/components/Header/Header.js
+++ views/site/src/components/Header/Header.js
@@ -7,10 +7,11 @@ import { Link } from 'react-router-dom';
 export default function Header({ pages }) {
   const keyList = new KeyList();
 
+  let pageCount = 0;
   const linkComponents = pages.map(page => {
     return (
-      <li className="nav-item" key={keyList.generateKey(page)}>
-        <Link to={page} className="nav-link">
+      <li key={keyList.generateKey(page)} className="nav-item">
+        <Link to={page} className="nav-link" >
           {page.replace(page[0], page[0].toUpperCase())}
         </Link>
       </li>
@@ -18,27 +19,20 @@ export default function Header({ pages }) {
   });
 
   return (
-    <nav className="navbar navbar-expand-md bg-primary">
-      <div id="pages" className="container-fluid">
-        <Link className="navbar-brand" to="/" >
-          <img src={brand} height={70}/>
+    <header className="navbar navbar-expand-md bg-primary ">
+      <div className="container-fluid mx-2">
+        <Link className="navbar-brand d-inline-block" to="/" >
+          <img src={brand} height={70} alt="bakery" />
         </Link>
-        <button 
-          className="navbar-toggler" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#pages" 
-          aria-controls="pages" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#pages" aria-controls="pages" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="pages">
+        <div className="collapse navbar-collapse justify-content-end" id="pages">
           <ul className="navbar-nav">
             {linkComponents}
           </ul>
         </div>
       </div>
-    </nav>
+    </header>
   );
 }
