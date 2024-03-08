02a3bed95043357d30fcc258fa319cd5f5df2fbb
mb1721 <masonbennett1721@gmail.com>
2024-03-07 19:13:20 -0500

    Stylize header

--- views/site/src/components/Header/Header.js
+++ views/site/src/components/Header/Header.js
@@ -11,23 +11,23 @@ export default function Header({ pages }) {
   let pageCount = 0;
   const linkComponents = pages.map(page => {
     return (
-      <Link key={keyList.generateKey(page)} to={page} className="nav-link" >
-        <h2>{page.replace(page[0], page[0].toUpperCase())}</h2>
+      <Link key={keyList.generateKey(page)} to={page} className="nav-link d-inline-block flex-grow-1">
+        <h1>{page.replace(page[0], page[0].toUpperCase())}</h1>
       </Link>
     );
   });
 
   return (
     <header className="navbar navbar-expand-md bg-primary ">
-      <div className="container-fluid mx-2">
-        <Link className="navbar-brand d-inline-block" to="/" >
+      <div className="container-fluid mx-5">
+        <Link className="navbar-brand d-inline-block flex-grow-1" to="/" >
           <img src={brand} height={70} alt="bakery" />
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#pages" aria-controls="pages" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="pages">
-          <nav className="navbar-nav">
+          <nav className="navbar-nav w-100">
             {linkComponents}
           </nav>
         </div>
