04bbe42ba6ce1f1920b91d1bf58b2b5c512f0193
mb1721 <masonbennett1721@gmail.com>
2024-03-11 00:45:58 -0400

    Style gallery carousel

--- views/site/src/pages/Gallery.js
+++ views/site/src/pages/Gallery.js
@@ -24,7 +24,7 @@ export default function Gallery() {
       let newObj = { ...prevObj };
       let { gallery, carousel, panels } = newObj;
       
-      // fill gallery productGroups
+      // fill gallery productImgs
       galleryCtx.keys().forEach(img => {
         // fill product category set
         const category = img.split('/')[1];
@@ -38,38 +38,40 @@ export default function Gallery() {
       const colKeys = new KeyList();
       let newPanels = [];
       
-      const groupsPerSlide = 3;
-      const productGroups = Object.entries(carousel); 
-      let groupIdx = 0;
-      let numSlides = Math.ceil(productGroups.length / groupsPerSlide);
+      const imgsPerSlide = 3;
+      const productImgs = Object.entries(carousel); 
+      const imgSize = Math.floor(100/imgsPerSlide);
+
+      let imgIdx = 0;
+      let numSlides = Math.ceil(productImgs.length / imgsPerSlide);
     
       for (numSlides; numSlides >= 1; numSlides--) {
         // construct columns
         let cols = [];
-        for (let count = 0; count < groupsPerSlide; count++) {
+        for (let count = 0; count < imgsPerSlide; count++) {
           // if another image exits, push next image index into row 
-          if (productGroups[groupIdx]) {
-            const tuple = productGroups[groupIdx++];
+          if (productImgs[imgIdx]) {
+            const tuple = productImgs[imgIdx++];
             const productSrc = tuple[1];
             cols.push(
               <img
                 key={colKeys.generateKey(productSrc)} 
                 src={galleryCtx(productSrc)} 
-                className='col img-fluid'
+                className='col img-fluid p-0' style={{width: `${imgSize}%`}}
                 alt={camelCaseContext(productSrc).fileStr} 
               />
             );
           } else {
-            let blankSpots = groupsPerSlide - count;
+            let blankSpots = imgsPerSlide - count;
             let repeatIdx = 0;
             for (blankSpots; blankSpots > 0; blankSpots--) {
-              const repeatSrc = productGroups[repeatIdx++][1];
+              const repeatSrc = productImgs[repeatIdx++][1];
               // push slides at the front of the carousel to the back
               cols.push(
                 <img
                   key={colKeys.generateKey(repeatSrc)} 
                   src={galleryCtx(repeatSrc)} 
-                  className='col img-fluid'
+                  className='col img-fluid p-0' style={{width: `${imgSize}%`}}
                   alt={camelCaseContext(repeatSrc).fileStr} 
                 />
               );
@@ -79,7 +81,7 @@ export default function Gallery() {
         }
 
         let newPanel = (
-          <Carousel.Item key={panelKeys.generateKey('group')} className='container-fluid h-100'>
+          <Carousel.Item key={panelKeys.generateKey('Img')} className='container-fluid h-100'>
             <div className='row h-100'>
               {cols}
             </div>
@@ -98,7 +100,12 @@ export default function Gallery() {
 
   return (
     <div id="gallery">
-      <Carousel id='gallery-categories' interval={null} slide={false}>
+      <Carousel id='gallery-categories' 
+        interval={null} 
+        slide={false} 
+        data-bs-theme='dark' 
+        indicators={false}
+      >
         {navigation.panels}
       </Carousel>
     </div>
