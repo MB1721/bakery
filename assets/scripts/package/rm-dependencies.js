bc52502934fc1e28887e8bb87cc761634e0535f0
mb1721 <masonbennett1721@gmail.com>
2024-03-14 18:28:00 -0400

    Update rm-dependencies.js

--- assets/scripts/package/rm-dependencies.js
+++ assets/scripts/package/rm-dependencies.js
@@ -7,7 +7,7 @@ switch (platform) {
     exec("rm -r views/*/node_modules views/*/package-lock.json node_modules package-lock.json");
     break;
   case 'win32':
-    exec("del package-lock.json & rd /Q /S node_modules & FOR /D %d IN (views\*) DO (del %d\package-lock.json & rd /Q /S %d\node_modules)");
+    exec("del package-lock.json && rd /Q /S node_modules && FOR /D %d IN (views\\*) DO (del \"%d\\package-lock.json\" && rd /Q /S \"%d\\node_modules\")");
     break;
   default: 
     throw new Error('Unsupported platform');
